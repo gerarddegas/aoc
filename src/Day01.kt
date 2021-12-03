4ac4c7b291c20bc8b69f76ebf6741ed2619f1cc9
Gerard Degas <gerard.degas@afterpay.com>
2021-12-02 21:37:17 -0800

    finished day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        for (i in input.indices) {
+            if(i > 0) {
+                if(input[i] > input[i - 1]) {
+                    count+=1
+                }
+            }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var sum = input[0].toInt() + input[1].toInt() + input[2].toInt()
+        for(i in 1..input.size - 3) {
+            var current = input[i].toInt()
+            // get the sum of the next 3
+            val nextThreeSum = current + input[i+1].toInt() + input[i+2].toInt()
+            if(nextThreeSum > sum) {
+                count+=1
+            }
+            sum = nextThreeSum
+        }
+        return count
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
