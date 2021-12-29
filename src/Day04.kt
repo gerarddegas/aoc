dae7ad7fa9db07341d6ada8e5797e0aa1b58a1d0
Gerard Degas <gerard.degas@afterpay.com>
2021-12-28 18:38:28 -0800

    day 4 pooping on me

--- src/Day04.kt
+++ src/Day04.kt
@@ -119,11 +119,71 @@ fun main() {
         throw RuntimeException("No winning board :(")
     }
 
-    fun part2(input: List<String>) {
+    fun findAndRemoveWinningBoardIdx(boards:  MutableList<MutableList<MutableList<Int>>>, guesses: List<Int>): Int {
+        for (l in guesses.indices) {
+            val currGuess = guesses[l]
+            for (k in boards.indices) {
+                val board = boards[k]
+                for (i in board.indices) {
+                    val row = board[i]
+                    var hasMatchingNumber = false
+                    for (j in row.indices) {
+                        val num = row[j]
+                        if (num === currGuess) {
+                            hasMatchingNumber = true
+                            board[i][j] = -1
+                        }
+                    }
+                    if (hasMatchingNumber) {
+                        if (isWinningBoard(board)) {
+                            if (boards.size == 1) {
+                                return sumUpBoard(boards[0]) * currGuess
+                            }
+                            val res = boards.filterIndexed { idx, _ ->
+                                idx != k
+                            }.toMutableList()
+                            return findAndRemoveWinningBoardIdx(res, guesses.slice(l + 1 until guesses.size))
+                        }
+                    }
+                }
+            }
+        }
+        throw RuntimeException("No winning board :(")
     }
 
+    fun part2(input: List<String>): Int {
+        /*
+         1. find winning board and return index
+         2. remove it from list
+         3. repeat
+         4. stop condition is if one board exists
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+                                    i
+22 13* 17* 11*  0*
+ 8  2 23*  4* 24
+21*  9* 14* 16*  7*
+ 6 10*  3 18  5*
+ 1 12 20 15 19
+
+ 3 15  0*  2* 22
+ 9* 18 13* 17*  5*
+19  8  7* 25 23*
+20 11* 10* 24*  4*
+14* 21* 16* 12  6
+
+//14* 21* 17 24*  4*
+//10 16* 15  9* 19
+//18  8 23* 26 20
+//22 11* 13*  6  5*
+// 2* 0* 12  3  7*
+         */
+        val (numGuesses, boards) = splitUpInput(input)
+        return findAndRemoveWinningBoardIdx(boards, numGuesses)
+    }
+
+
     val input = readInput("Day04")
-    println(part1(input))
+//    println(part1(input))
     println(part2(input))
 }
 
